bb8897c41d2779aacd13a19bf085b0aa3e0a7266
ShanshanDu <nicoleflopy@gmail.com>
2013-05-10 01:38:50 +0800

    #8 Open with google navigation

--- src/com/ezhang/pop/navigation/GoogleNavigationApp.java
+++ src/com/ezhang/pop/navigation/GoogleNavigationApp.java
@@ -1,7 +1,5 @@
 package com.ezhang.pop.navigation;
 
-import java.util.Locale;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
@@ -13,10 +11,11 @@ class GoogleNavigationApp extends NavigationApp {
 
 	public void CallNavigateApp(Activity activity, String srcLatitude,
 			String srcLongitude, String destLatitudue, String destLongitude) {
-		String url = String.format(Locale.ENGLISH,
-				"http://maps.google.com/maps?saddr=%s,%s&daddr=%s,%s&directionsmode=driving",
-				srcLatitude, srcLongitude, destLatitudue, destLongitude);
-		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+		String uriString = String.format("geo:0,0?q=%s,%s", destLatitudue,
+				destLongitude);
+		Uri uri = Uri.parse(uriString);
+		Intent intent = new Intent(android.content.Intent.ACTION_VIEW, uri);
+		activity.startActivity(intent);
 		intent.setClassName("com.google.android.apps.maps",
 				"com.google.android.maps.MapsActivity");
 		activity.startActivity(intent);
