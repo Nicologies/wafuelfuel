49894ecbbc902b100357cfe048889d9a9f363dae
ShanshanDu <nicoleflopy@gmail.com>
2013-06-01 13:32:31 +0800

    #28 Crash when showing cur address

--- src/com/ezhang/pop/ui/MainActivity.java
+++ src/com/ezhang/pop/ui/MainActivity.java
@@ -39,19 +39,19 @@ public class MainActivity extends Activity implements Observer {
     /**
      * There's no need to continue if switched to another activity
      */
-    enum EmIndication{
+    enum EmIndication {
         EmContinue,
         EmStop
     }
 
     /**
      * Manage requests that query data from remote service.
-     * */
-	private RequestManager m_reqManager;
-	private LocationManager m_locationManager;
-	private FuelStateMachine m_fuelStateMachine;
-	private ArrayList<FuelDistanceItem> m_fuelInfoList = new ArrayList<FuelDistanceItem>();
-	private ListView m_fuelDistanceItemlistView = null;
+     */
+    private RequestManager m_reqManager;
+    private LocationManager m_locationManager;
+    private FuelStateMachine m_fuelStateMachine;
+    private ArrayList<FuelDistanceItem> m_fuelInfoList = new ArrayList<FuelDistanceItem>();
+    private ListView m_fuelDistanceItemlistView = null;
 
     private Button m_refreshButton = null;
     private AnimationDrawable m_refreshButtonAnimation = null;
@@ -63,28 +63,28 @@ public class MainActivity extends Activity implements Observer {
     /**
      * The user will be required to choose the location provider type: location service or set the location manually.
      * This variable is to prevent the selection dialog appearing again on again as the app might be resumed several times during the first session.
-     * */
+     */
     private boolean m_locationTypeSelected = false;
     private Toast m_toast;
     private Bundle m_savedInstanceState;
 
     @Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_activity);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
 
-		m_reqManager = RequestManager.from(this);
-		m_refreshButton = (Button) findViewById(R.id.RefreshButton);
-		m_refreshButtonAnimation = (AnimationDrawable) m_refreshButton
-				.getBackground();
+        m_reqManager = RequestManager.from(this);
+        m_refreshButton = (Button) findViewById(R.id.RefreshButton);
+        m_refreshButtonAnimation = (AnimationDrawable) m_refreshButton
+                .getBackground();
 
-		if (savedInstanceState != null) {
+        if (savedInstanceState != null) {
             m_savedInstanceState = savedInstanceState;
-		}
+        }
 
         InitFuelDistanceItemListView();
         m_settings = new AppSettings(this);
-	}
+    }
 
     private void InitFuelDistanceItemListView() {
         m_fuelDistanceItemlistView = (ListView) this.findViewById(R.id.fuelDistanceItemlistView);
@@ -103,35 +103,35 @@ public class MainActivity extends Activity implements Observer {
     }
 
     public void OnRefreshClicked(View v) {
-		if (this.m_fuelStateMachine != null) {
-			this.m_fuelStateMachine.Refresh();
-		}
-	}
+        if (this.m_fuelStateMachine != null) {
+            this.m_fuelStateMachine.Refresh();
+        }
+    }
 
-	public void OnSettingsClicked(View v) {
-		Intent intent = new Intent(this, SettingsActivity.class);
-		this.startActivity(intent);
-	}
+    public void OnSettingsClicked(View v) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        this.startActivity(intent);
+    }
 
-	public void OnChangeLocationClicked(View v) {
-		Intent intent = new Intent(this, CustomLocationActivity.class);
-		this.startActivity(intent);
-	}
+    public void OnChangeLocationClicked(View v) {
+        Intent intent = new Intent(this, CustomLocationActivity.class);
+        this.startActivity(intent);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-        if(m_fuelStateMachine != null){
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        if (m_fuelStateMachine != null) {
             m_fuelStateMachine.SaveInstanceState(outState);
         }
-		super.onSaveInstanceState(outState);
-	}
+        super.onSaveInstanceState(outState);
+    }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
@@ -139,38 +139,37 @@ public class MainActivity extends Activity implements Observer {
         super.onRestoreInstanceState(savedInstanceState);
     }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-		m_settings.LoadDiscountSettings(new ICallable<Object, Object>() {
-			public Object Call(Object o) {
-				OnDiscountInfoLoaded();
-				return o;
-			}
-		});
-	}
+        m_settings.LoadDiscountSettings(new ICallable<Object, Object>() {
+            public Object Call(Object o) {
+                OnDiscountInfoLoaded();
+                return o;
+            }
+        });
+    }
 
-	private void OnDiscountInfoLoaded() {
-		if (m_locationManager == null) {
-			m_locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		}
+    private void OnDiscountInfoLoaded() {
+        if (m_locationManager == null) {
+            m_locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        }
 
         PromptEnableNetwork();
 
-        if (PromptLocationTypeSelection() == EmIndication.EmStop){
+        if (PromptLocationTypeSelection() == EmIndication.EmStop) {
             return;
         }
 
         OnLocationTypeSelected();
-	}
+    }
 
     private EmIndication PromptLocationTypeSelection() {
         boolean isFirstRun = this.m_settings.IsFirstRun();
-        if(isFirstRun && !m_locationTypeSelected)
-        {
+        if (isFirstRun && !m_locationTypeSelected) {
             CreateGPSOrCustomLocationAlertDlg();
-            if(!m_gpsOrCustomLocationDlg.isShowing()){
+            if (!m_gpsOrCustomLocationDlg.isShowing()) {
                 m_gpsOrCustomLocationDlg.show();
             }
             return EmIndication.EmStop;
@@ -181,7 +180,7 @@ public class MainActivity extends Activity implements Observer {
     private void OnLocationTypeSelected() {
         boolean useGPS = m_settings.UseGPSAsLocation();
         if (useGPS) {
-            if (PromptEnableLocationService() == EmIndication.EmStop){
+            if (PromptEnableLocationService() == EmIndication.EmStop) {
                 return;
             }
         }
@@ -197,7 +196,7 @@ public class MainActivity extends Activity implements Observer {
     }
 
     private void RestoreFromSavedInstance() {
-        if(m_savedInstanceState != null && m_fuelStateMachine != null){
+        if (m_savedInstanceState != null && m_fuelStateMachine != null) {
             m_fuelStateMachine.RestoreFromSaveInstanceState(m_savedInstanceState);
             m_savedInstanceState = null;
         }
@@ -261,8 +260,8 @@ public class MainActivity extends Activity implements Observer {
         return EmIndication.EmContinue;
     }
 
-    private void CreateGPSOrCustomLocationAlertDlg(){
-        if(m_gpsOrCustomLocationDlg != null){
+    private void CreateGPSOrCustomLocationAlertDlg() {
+        if (m_gpsOrCustomLocationDlg != null) {
             return;
         }
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
@@ -301,106 +300,107 @@ public class MainActivity extends Activity implements Observer {
     }
 
     private void CreateLocationAccessAlertDlg() {
-		if (m_locationAccessDlg != null) {
-			return;
-		}
-		AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        if (m_locationAccessDlg != null) {
+            return;
+        }
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
 
-		// Setting Dialog Title
-		alertDialog.setTitle("Location Access Required");
+        // Setting Dialog Title
+        alertDialog.setTitle("Location Access Required");
 
-		// Setting Dialog Message
-		alertDialog
-				.setMessage("Location Access is disabled. Press go to the settings menu and enable both \n * GPS satellites\n * Wi-Fi & mobile network");
+        // Setting Dialog Message
+        alertDialog
+                .setMessage("Location Access is disabled. Press go to the settings menu and enable both \n * GPS satellites\n * Wi-Fi & mobile network");
 
-		// On pressing Settings button
-		alertDialog.setPositiveButton("Go",
-				new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int which) {
-						Intent intent = new Intent(
-								Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-						MainActivity.this.startActivity(intent);
-					}
-				});
+        // On pressing Settings button
+        alertDialog.setPositiveButton("Go",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(
+                                Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        MainActivity.this.startActivity(intent);
+                    }
+                });
 
-		// Showing Alert Message
-		m_locationAccessDlg = alertDialog.create();
-	}
+        // Showing Alert Message
+        m_locationAccessDlg = alertDialog.create();
+    }
 
-	private void CreateNetworkAlertDlg() {
-		if (m_networkAlertDlg != null) {
-			return;
-		}
-		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+    private void CreateNetworkAlertDlg() {
+        if (m_networkAlertDlg != null) {
+            return;
+        }
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
 
-		// Setting Dialog Title
-		alertDialogBuilder.setTitle("Network Access Required");
+        // Setting Dialog Title
+        alertDialogBuilder.setTitle("Network Access Required");
 
-		// Setting Dialog Message
-		alertDialogBuilder
-				.setMessage("Network is not enabled. Please either enable Wi-Fi or Mobile Network Data");
+        // Setting Dialog Message
+        alertDialogBuilder
+                .setMessage("Network is not enabled. Please either enable Wi-Fi or Mobile Network Data");
 
-		// On pressing Settings button
-		alertDialogBuilder.setPositiveButton("Wi-Fi",
-				new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int which) {
-						Intent intent = new Intent(
-								Settings.ACTION_WIFI_SETTINGS);
-						MainActivity.this.startActivity(intent);
-					}
-				});
+        // On pressing Settings button
+        alertDialogBuilder.setPositiveButton("Wi-Fi",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(
+                                Settings.ACTION_WIFI_SETTINGS);
+                        MainActivity.this.startActivity(intent);
+                    }
+                });
 
-		// On pressing Settings button
-		alertDialogBuilder.setNegativeButton("MobileNetwork",
-				new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int which) {
-						Intent intent = new Intent(
-								Settings.ACTION_DATA_ROAMING_SETTINGS);
-						// intent.setClassName("com.android.phone",
-						// "com.android.phone.Settings");
-						MainActivity.this.startActivity(intent);
-					}
-				});
+        // On pressing Settings button
+        alertDialogBuilder.setNegativeButton("MobileNetwork",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(
+                                Settings.ACTION_DATA_ROAMING_SETTINGS);
+                        // intent.setClassName("com.android.phone",
+                        // "com.android.phone.Settings");
+                        MainActivity.this.startActivity(intent);
+                    }
+                });
 
-		// Showing Alert Message
-		m_networkAlertDlg = alertDialogBuilder.create();
-	}
+        // Showing Alert Message
+        m_networkAlertDlg = alertDialogBuilder.create();
+    }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-	}
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
 
     /**
      * Handles the notifications from StateMachine, then update the UI accordingly.
+     *
      * @param arg0
      * @param arg1
      */
-	@Override
-	public void update(Observable arg0, Object arg1) {
-		if (this.m_fuelStateMachine.GetCurState() == EmState.DistanceReceived) {
+    @Override
+    public void update(Observable arg0, Object arg1) {
+        if (this.m_fuelStateMachine.GetCurState() == EmState.DistanceReceived) {
             OnDistanceInfoReceived();
-		}
-		if (this.m_fuelStateMachine.GetCurState() == EmState.SuburbReceived) {
+        }
+        if (this.m_fuelStateMachine.GetCurState() == EmState.SuburbReceived) {
             OnSuburbReceived();
-		}
+        }
 
-		if (this.m_fuelStateMachine.GetCurState() == EmState.FuelInfoReceived) {
+        if (this.m_fuelStateMachine.GetCurState() == EmState.FuelInfoReceived) {
             OnFuelInfoReceived();
-		}
+        }
 
-		if (this.m_fuelStateMachine.GetCurState() == EmState.Start) {
+        if (this.m_fuelStateMachine.GetCurState() == EmState.Start) {
             OnStateMachineStarted();
-		}
+        }
 
-		if (this.m_fuelStateMachine.GetCurState() == EmState.GeoLocationReceived) {
+        if (this.m_fuelStateMachine.GetCurState() == EmState.GeoLocationReceived) {
             OnLocationReceived();
-		}
+        }
 
-		if (this.m_fuelStateMachine.GetCurState() == EmState.Timeout) {
+        if (this.m_fuelStateMachine.GetCurState() == EmState.Timeout) {
             OnTimeout();
-		}
-	}
+        }
+    }
 
     /**
      * Handles timeout event from the StateMachine.
@@ -462,71 +462,72 @@ public class MainActivity extends Activity implements Observer {
     }
 
     private void ShowCurrentAddr() {
-		if (this.m_fuelStateMachine.m_suburb != null
-				&& this.m_fuelStateMachine.m_suburb != "") {
-			int indexOfSuburb = this.m_fuelStateMachine.m_address
-					.indexOf(this.m_fuelStateMachine.m_suburb);
+        if (this.m_fuelStateMachine.m_suburb != null
+                && this.m_fuelStateMachine.m_suburb != "") {
+            int indexOfSuburb = this.m_fuelStateMachine.m_address
+                    .indexOf(this.m_fuelStateMachine.m_suburb);
+            String addrWithoutSuburb = this.m_fuelStateMachine.m_address;
+            if (indexOfSuburb > 0) {
+                addrWithoutSuburb = this.m_fuelStateMachine.m_address
+                        .substring(0, indexOfSuburb);
+            }
+            TextView v = ((TextView) this.findViewById(R.id.curAddressText));
+            String addrText = String.format(Locale.ENGLISH,
+                    "You're at:%s%s.Touch here to change if incorrect.",
+                    addrWithoutSuburb, this.m_fuelStateMachine.m_suburb);
+            v.setText(addrText);
 
-			String addrWithoutSuburb = this.m_fuelStateMachine.m_address
-					.substring(0, indexOfSuburb);
-			TextView v = ((TextView) this.findViewById(R.id.curAddressText));
-			String addrText = String.format(Locale.ENGLISH,
-					"You're at:%s%s.Touch here to change if incorrect.",
-					addrWithoutSuburb, this.m_fuelStateMachine.m_suburb);
-			v.setText(addrText);
+            View curAddr = this.findViewById(R.id.layoutCurAddr);
+            curAddr.setVisibility(View.VISIBLE);
+        }
+    }
 
-			View curAddr = this.findViewById(R.id.layoutCurAddr);
-			curAddr.setVisibility(View.VISIBLE);
-		}
-	}
+    private void HideCurrentAddress() {
+        View curAddr = this.findViewById(R.id.layoutCurAddr);
+        curAddr.setVisibility(View.GONE);
+    }
 
-	private void HideCurrentAddress() {
-		View curAddr = this.findViewById(R.id.layoutCurAddr);
-		curAddr.setVisibility(View.GONE);
-	}
+    private void SwitchToStopWaiting() {
+        m_refreshButtonAnimation.stop();
+        m_refreshButton.setEnabled(true);
+    }
 
-	private void SwitchToStopWaiting() {
-		m_refreshButtonAnimation.stop();
-		m_refreshButton.setEnabled(true);
-	}
+    private void SwitchToWaitingStatus() {
+        m_refreshButtonAnimation.start();
+        m_refreshButton.setEnabled(false);
+    }
 
-	private void SwitchToWaitingStatus() {
-		m_refreshButtonAnimation.start();
-		m_refreshButton.setEnabled(false);
-	}
+    public void OnShareWithFriendClicked(View v) {
+        String url = String.format(
+                "https://play.google.com/store/apps/details?id=%s",
+                getApplicationContext().getPackageName());
+        String content = "Hey, I'm using WaFuelFuel to help find the cheapest and nearest petrol station in Western Australia.\nCheck it out in GooglePlay: "
+                + url;
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, content);
+        startActivity(shareIntent);
+    }
 
-	public void OnShareWithFriendClicked(View v) {
-		String url = String.format(
-				"https://play.google.com/store/apps/details?id=%s",
-				getApplicationContext().getPackageName());
-		String content = "Hey, I'm using WaFuelFuel to help find the cheapest and nearest petrol station in Western Australia.\nCheck it out in GooglePlay: "
-				+ url;
-		Intent shareIntent = new Intent(Intent.ACTION_SEND);
-		shareIntent.setType("text/plain");
-		shareIntent.putExtra(Intent.EXTRA_TEXT, content);
-		startActivity(shareIntent);
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle item selection
-		switch (item.getItemId()) {
-		case R.id.action_settings:
-			OnSettingsClicked(null);
-			return true;
-		case R.id.share_with_friend_menu:
-			this.OnShareWithFriendClicked(null);
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                OnSettingsClicked(null);
+                return true;
+            case R.id.share_with_friend_menu:
+                this.OnShareWithFriendClicked(null);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     private void ShowStatusText(String text) {
-        if(m_toast == null){
+        if (m_toast == null) {
             m_toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
-        }
-        else{
+        } else {
             m_toast.setText(text);
         }
         m_toast.show();
